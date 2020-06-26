c6483abe66dd6e4d45f3214516ebb55b79b3870d
jivii <67268647+jivii@users.noreply.github.com>
2020-06-26 11:59:40 +0530

    Update card.js

--- views/card.js
+++ views/card.js
@@ -34,3 +34,24 @@ form.addEventListener('submit', e => {
     else stripeTokenHandler(res.token);
   })
 })
+app.post("/charge", (req, res) => {
+  try {
+    stripe.customers
+      .create({
+        name: req.body.name,
+        email: req.body.email,
+        source: req.body.stripeToken
+      })
+      .then(customer =>
+        stripe.charges.create({
+          amount: req.body.amount * 100,
+          currency: "usd",
+          customer: customer.id
+        })
+      )
+      .then(() => res.render("completed.html"))
+      .catch(err => console.log(err));
+  } catch (err) {
+    res.send(err);
+  }
+});
