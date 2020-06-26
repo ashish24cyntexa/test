d816b884c7536d15af408c4dc8292356148d5958
jivii <67268647+jivii@users.noreply.github.com>
2020-06-26 17:47:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,5 +24,25 @@ app.listen(port, function() {
 	console.log('Our app is running on http://localhost:' + port);
 });
 
-
+app.post("/charge", (req, res) => {
+  try {
+    stripe.customers
+      .create({
+        name: req.body.name,
+        email: req.body.email,
+        source: req.body.stripeToken
+      })
+      .then(customer =>
+        stripe.charges.create({
+          amount: req.body.amount * 100,
+          currency: "usd",
+          customer: customer.id
+        })
+      )
+      .then(() => res.render("completed.html"))
+      .catch(err => console.log(err));
+  } catch (err) {
+    res.send(err);
+  }
+});
 
