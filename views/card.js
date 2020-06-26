44988ad6c9851394fcb990b95198a72b642f139b
jivii <67268647+jivii@users.noreply.github.com>
2020-06-26 12:46:51 +0530

    Update card.js

--- views/card.js
+++ views/card.js
@@ -1,35 +1,6 @@
-const stripe = Stripe('pk_test_cpavqkvHKukifq3zixaAncGf00w1wjeAsB'); // Your Publishable Key
-const elements = stripe.elements();
+var stripe = require('stripe')('sk_test_4UNeAg7ULlqyKdIUjHj0hx8G002Zx3RDDz');
 
-// Create our card inputs
-var style = {
-  base: {
-    color: "#fff"
-  }
-};
-
-const card = elements.create('card', { style });
-card.mount('#card-element');
-
-const form = document.querySelector('form');
-const errorEl = document.querySelector('#card-errors');
-
-// Give our token to our form
-const stripeTokenHandler = token => {
-  const hiddenInput = document.createElement('input');
-  hiddenInput.setAttribute('type', 'hidden');
-  hiddenInput.setAttribute('name', 'stripeToken');
-  hiddenInput.setAttribute('value', token.id);
-  form.appendChild(hiddenInput);
-
-  form.submit();
-}
-
-// Create token from card data
-form.addEventListener('submit', e => {
-  e.preventDefault();
-  
-  stripe.customers.create(
+stripe.customers.create(
   {
     description: 'My First Test Customer (created for API docs)',
   },
@@ -37,7 +8,3 @@ form.addEventListener('submit', e => {
     // asynchronously called
   }
 );
-
-  
-})
-
